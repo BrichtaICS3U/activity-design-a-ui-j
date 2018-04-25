01303f7ab04b46a8bcabdc87026169bf3fa62366
JESSE GURAM <s328155668@ocdsb>
2018-04-25 09:42:20 -0400

    activity

--- menuTemplateButtonClass.py
+++ menuTemplateButtonClass.py
@@ -11,8 +11,8 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
-SCREENWIDTH = 120
-SCREENHEIGHT = 100
+SCREENWIDTH = 400
+SCREENHEIGHT = 380
 size = (SCREENWIDTH, SCREENHEIGHT)
 screen = pygame.display.set_mode(size)
 
@@ -66,8 +66,16 @@ def my_shell_function():
     """A generic function that prints something in the shell"""
     print('Fire the nukes!')
 
-def my_next_function():
-    """A function that advances to the next level"""
+def my_hello_function():
+   print('Hello!')
+
+def my_sound_function():
+    print('Sound')
+
+
+
+def my_settings_function():
+    """A function that advances to settings shell"""
     global level
     level += 1
 
@@ -75,6 +83,14 @@ def my_previous_function():
     """A function that retreats to the previous level"""
     global level
     level -= 1
+
+def my_on_function():
+    global level
+    print("Sound On")
+
+def my_off_function():
+    global level
+    print("Sound Off")
 
 def my_quit_function():
     """A function that will quit the game and close the pygame window"""
@@ -98,13 +114,16 @@ carryOn = True
 clock = pygame.time.Clock()
 
 #create button objects
-button_01 = Button("Next", (SCREENWIDTH/2, SCREENHEIGHT/3), my_next_function)
-button_02 = Button("Previous", (SCREENWIDTH/2, SCREENHEIGHT/3), my_previous_function)
-button_03 = Button("Quit", (SCREENWIDTH/2, SCREENHEIGHT*2/3), my_quit_function, bg=(50, 200, 20))
-
+button_Hello = Button("Hello", (SCREENWIDTH/2, SCREENHEIGHT/4),my_hello_function)
+button_Previous = Button("Previous", (SCREENWIDTH/2, SCREENHEIGHT*3/4), my_previous_function)
+button_Quit = Button("Quit", (SCREENWIDTH/2, SCREENHEIGHT*3/4), my_quit_function, bg=(50, 200, 20))
+button_Settings = Button("Settings", (SCREENWIDTH/2, SCREENHEIGHT/2),my_settings_function)
+button_Sound = Button("Sound", (SCREENWIDTH/2, SCREENHEIGHT/4), my_sound_function)
+button_On = Button("ON", (SCREENWIDTH/2, SCREENHEIGHT/3), my_on_function)
+button_Off = Button("OFF", (SCREENWIDTH/2, SCREENHEIGHT/2), my_off_function)
 #arrange button groups depending on level
-level1_buttons = [button_01, button_03]
-level2_buttons = [button_02, button_03]
+level1_buttons = [button_Settings, button_Hello, button_Quit]
+level2_buttons = [button_Sound, button_Previous,button_On, button_Off]
 
 #---------Main Program Loop----------
 while carryOn:
